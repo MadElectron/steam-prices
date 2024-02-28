141539ebac9d5943ee359e2c20631d0e48ba6418
Denis Bondarenko <danbond85@mail.ru>
2024-02-28 17:33:59 +0300

    Error messages translated

--- app/functions.js
+++ app/functions.js
@@ -4,32 +4,41 @@ import {
   getSteamExchangeRateKZT,
   getSteamGamePriceOverview
 } from './steamApi.js'
-import { STEAM_COMMISSION_PERCENT, VOLATILITY_PERCENT } from './variables.js'
+import {
+  STEAM_COMMISSION_PERCENT,
+  TRANSLATIONS,
+  VOLATILITY_PERCENT
+} from './variables.js'
 import { getPlatiResult } from './platiApi.js'
 
-export const getPrice = async (name) => {
+/**
+ * Asynchronously fetches the price and related information for a game.
+ *
+ * @param {string} name - The name of the game to search for.
+ * @param {string} [lang='ru'] - The language code for the translations (default: 'ru').
+ * @return {Object} An object containing the information about the game price and related details.
+ */
+export const getPrice = async (name, lang = 'ru') => {
   const game = await searchGame(name)
 
-  if (!game) return { ok: false, error: 'Game not found' }
+  if (!game) return { ok: false, error: TRANSLATIONS.gameNotFound[lang] }
 
   const title = game.name
   const steamPriceOverview = await getSteamGamePriceOverview(game.appid)
 
   if (!steamPriceOverview)
-    return { ok: false, error: 'Game does not have price overview' }
+    return { ok: false, error: TRANSLATIONS.noPriceOverview[lang] }
 
   const steamPriceKZT = steamPriceOverview.final / 100
   const steamPriceKZTCommissioned =
     steamPriceKZT * (1 + STEAM_COMMISSION_PERCENT / 100)
   const rubRate = await getExchengeRateRub()
 
-  if (!rubRate)
-    return { ok: false, error: 'Cannot get Tsentrobank exchange rate' }
+  if (!rubRate) return { ok: false, error: TRANSLATIONS.noCBRRate[lang] }
 
   const steamKZTRate = await getSteamExchangeRateKZT()
 
-  if (!steamKZTRate)
-    return { ok: false, error: 'Cannot get Steam exchange rate' }
+  if (!steamKZTRate) return { ok: false, error: TRANSLATIONS.noSteamRate[lang] }
 
   const steamPriceUSD = steamPriceKZTCommissioned / steamKZTRate
   const steamPriceRUB = Math.ceil(steamPriceUSD * rubRate)
--- app/variables.js
+++ app/variables.js
@@ -1,2 +1,21 @@
 export const STEAM_COMMISSION_PERCENT = 11
 export const VOLATILITY_PERCENT = 10
+
+export const TRANSLATIONS = {
+  gameNotFound: {
+    en: 'Game not found',
+    ru: 'Игра не найдена'
+  },
+  noPriceOverview: {
+    en: 'Game does not have Steam price overview',
+    ru: 'Игра не имеет ценника Steam'
+  },
+  noCBRRate: {
+    en: 'Cannot get Tsentrobank exchange rate',
+    ru: 'Не удалось получить курс Tsentrobank'
+  },
+  noSteamRate: {
+    en: 'Cannot get Steam exchange rate',
+    ru: 'Не удалось получить курс Steam'
+  }
+}
